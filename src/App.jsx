2772f172e912753534a255710f0fd9f2bc17ed39
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2024-11-25 21:01:53 +0100

    display result

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,170 @@
 import { useState } from 'react'
 import './App.css'
-import Form from './components/Form'
+import './index.css'
+// import Form from './components/Form'
 function App() {
-  // const [count, setCount] = useState(0)
+  const [inputFields, setInputFields] = useState({
+    mortgageAmount: "",
+    interestRate: "",
+    mortgageTerm: "",
+    mortgageType: null
+  });
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+  const [monthlyRepayment, setMonthlyRepayment] = useState("");
+  // const [totalRepayment, setTotalRepayment] = useState("");
+  // input form validation funtion
+  const validateValues = (inputValues) => {
+    let errors = {};
+    if (!inputValues.mortgageAmount) {
+      errors.mortgageAmount = "This field is required!";
+    }
+    if (!inputValues.interestRate) {
+      errors.interestRate = "This field is required!";
+    }
+    if (!inputValues.mortgageTerm) {
+      errors.mortgageTerm = "This field is required!"
+    }
+    if (!inputValues.mortgageType) {
+      errors.mortgageType = "This field is required!";
+    }
+    return errors;
+  };
+  // input onChange function
+  const handleChange = (e) => {
+    setInputFields({ ...inputFields, [e.target.name]: e.target.value });
+
+  };
+  // submit form calculate repayment function
+  const calculateRepayment = (event) => {
+    event.preventDefault();
+    setErrors(validateValues(inputFields));
+    setSubmitting(true);
+    setMonthlyRepayment(() => {
+      const amount = parseFloat(inputFields.mortgageAmount);
+      const rate = parseFloat(inputFields.interestRate) / 100 / 12;
+      const term = parseFloat(inputFields.mortgageTerm) * 12;
+      let repayment;
+      if (inputFields.mortgageType === 'repayment') {
+        repayment = (amount * rate) / (1 - Math.pow(1 + rate, -term))
+        console.log(repayment);
+      } else if (inputFields.mortgageType === 'interestOnly') {
+        repayment = amount * rate;
+        console.log(repayment);
+      }
+      setMonthlyRepayment(repayment ? repayment.toFixed(2) : null)
+      console.log(repayment);
+
+
+    })
+
+
+    console.log(inputFields)
+  }
+  // clear All function
+  const clearAll = () => {
+    setInputFields({
+      mortgageAmount: '',
+      mortgageTerm: '',
+      interestRate: '',
+      mortgageType: null
+    })
+    setMonthlyRepayment(null);
+  }
 
   return (
-    // <div className='flex justify-center items-center'>
     <div className='max-w-5xl mx-auto  my-48 rounded-3xl bg-white shadow-lg'>
       <div className='flex md:flex-row flex-col space-x-0 md:space-x-10  '>
-        <div className='w-1/2 pt-12 pl-6'>
+        <div className='w-1/2 pt-10 pl-6'>
           <div className='flex justify-between items-center '>
             <h1 className='text-3xl text-Neutral-slate-900 font-display font-semibold'>Mortgage calculator</h1>
-            <a href='/' className='underline text-lg text-Neutral-slate-700 font-display'>Clear All</a>
+            <button className='underline text-lg text-Neutral-slate-700 font-display' onClick={clearAll}>Clear All</button>
           </div>
-          <Form>
-          </Form>
+          <div className='pt-12'>
+            <form onSubmit={calculateRepayment}>
+              <div className='relative'>
+                <label htmlFor="mortgage amount" className='text-lg text-Neutral-slate-500 font-display font-medium'>Mortgage Amount</label>
+                < input type='text' value={inputFields.mortgageAmount} name='mortgageAmount' onChange={handleChange} className='border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-16 font-display font-semibold text-Neutral-slate-900 hover:border-Neutral-slate-900' style={{ border: errors.mortgageAmount ? "1px solid hsl(4, 69%, 50%)" : null }}
+                />
+                <span className={`${errors.mortgageAmount ? 'absolute left-0 bg-primary-red h-[46px] rounded-tl-md rounded-bl-md bottom-[2.05rem] font-display' : 'absolute left-0 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.55rem] font-display'}`}><p className={`${errors.mortgageAmount ? 'text-white px-4 py-3 font-bold' : 'px-4 py-3 font-bold text-Neutral-slate-500'}`}>£</p></span>
+
+                {errors.mortgageAmount ? (
+                  <p className="error text-primary-red">This Field is required!</p>
+                ) : null}
+              </div>
+
+              <div className='py-6 flex justify-between space-x-6' >
+                <div className='relative'>
+                  <label htmlFor="mortgage Term" className='text-lg text-Neutral-slate-500 font-display font-medium'>Mortgage Term</label>
+                  <input type='text' value={inputFields.mortgageTerm} name='mortgageTerm' onChange={handleChange} className='  border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-4 font-display font-semibold text-Neutral-slate-900 ' style={{ border: errors.mortgageTerm ? "1px solid hsl(4, 69%, 50%)" : null }} />
+                  <span className={`${errors.mortgageTerm ? 'absolute left-[9.12rem] bg-primary-red h-[46px] rounded-tl-md rounded-bl-md bottom-[2.05rem] font-display' : 'absolute right-[0.15rem] bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.6rem] font-display'}`}><p className={`${errors.mortgageTerm ? 'text-white px-5 py-3 font-bold' : 'px-4 py-3 font-bold text-Neutral-slate-500'}`}>years</p></span>
+                  {errors.mortgageTerm ? (
+                    <p className="error text-primary-red">This Field is required!</p>
+                  ) : null}
+                </div>
+
+                <div className='relative'>
+                  <label htmlFor="interest rate" className='text-lg text-Neutral-slate-500 font-display font-medium'>Interest Rate</label>
+                  <input type='text' value={inputFields.interestRate} name='interestRate' onChange={handleChange} className=' border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-4 font-display font-semibold text-Neutral-slate-900' style={{ border: errors.interestRate ? '1px solid hsl(4, 69%, 50%)' : null }} />
+
+                  <span className={`${errors.interestRate ? 'absolute right-[0.06rem] bg-primary-red h-[46px] rounded-tl-md rounded-bl-md top-[37px]' : 'absolute right-[0.15rem] bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md top-[37px] '}`} ><p className={`${errors.interestRate ? 'text-white px-4 py-3 font-bold' : 'px-4 py-3 font-display font-bold text-Neutral-slate-500'}`}>%</p></span>
+                  {errors.mortgageTerm ? (
+                    <p className="error text-primary-red">This Field is required!</p>
+                  ) : null}
+                </div>
+              </div>
+              <div>
+                <div className={`${inputFields.mortgageType === 'repayment' ? 'h-12 w-full border border-primary-lime rounded-md pt-2 pl-4' : 'h-12 w-full border border-Neutral-slate-500 rounded-md pt-2 pl-4 hover:border-primary-lime'}`} id='radio'>
+                  <input type='radio' value='repayment' name='mortgageType' onChange={handleChange} checked={inputFields.mortgageType === 'repayment'} className='w-4 h-4' />
+                  <label htmlFor="repayment" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Repayment</label>
+
+                </div>
+                <div className={`${inputFields.mortgageType === 'interestOnly' ? 'h-12 w-full border border-primary-lime rounded-md my-2 pt-2 pl-4' : 'h-12 w-full border border-Neutral-slate-500 rounded-md my-2 pt-2 pl-4'}`}>
+
+                  <input type="radio" value='interestOnly' name='mortgageType' onChange={handleChange} checked={inputFields.mortgageType === 'interestOnly'} className='h-4 w-4 text-Neutral-slate-500' id='radio' style={{ border: inputFields.mortgageType === "interestOnly" ? "1px solid red" : "white" }} />
+                  <label htmlFor="interest only" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Interest Only</label>
+                </div>
+                {errors.mortgageType ? (
+                  <p className="error text-primary-red">This Field is required!</p>
+                ) : null}
+              </div>
+              <button type='submit' className='bg-primary-lime text-Neutral-slate-900 rounded-3xl w-3/4 h-14 font-semibold font-display my-10 hover:opacity-75'>Calculate Repayments</button>
+            </form>
+          </div>
         </div>
         <div className='w-1/2 bg-Neutral-slate-900   rounded-bl-[100px] rounded-tr-3xl rounded-br-3xl h-full'>
-          <div className='flex justify-center items-center mt-36 mb-6 '>
-            <img src='/images/illustration-empty.svg' class='h-full items-center'></img>
-          </div>
-          <div className='px-8 pb-44'>
-            <h2 className='text-white text-2xl text-center font-display'>Results shown here</h2>
-            <p className='text-Neutral-slate-500 text-center  pt-4 font-display'>Complete the form and click "calculate repayments" to see what your monthly repayments will be </p>
-          </div>
+          {monthlyRepayment ?
+            <div>
+              <div className='px-8 pb-52 pt-10'>
+                <h2 className='text-white text-xl font-display'>Your Result</h2>
+                <p className='text-Neutral-slate-500 pt-4 font-display'>Your results are shown below based on the information you provided. To adjust the result edit the form and click "calculte repayments" again. </p>
+                <div className='relative bg-primary-lime h-48 rounded-md w-full mt-14'>
+                  <div className='absolute bg-Neutral-slate-900 shadow-lg rounded-md  top-[4px] w-full px-4'>
+                    <p className='pt-10 pb-3 text-Neutral-slate-300 font-display'>Your monthly repayment</p>
+                    <h2 className='text-primary-lime text-4xl  font-display'>£ {monthlyRepayment}</h2>
+                    <hr className='my-6 text-Neutral-slate-300'></hr>
+                    <p className='text-Neutral-slate-300 font-display pb-2 pt-[4px]'>Total you'll repay over the term</p>
+                    <p className='pb-8 text-Neutral-white text-lg font-display pt-[4px]'>£ </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            :
+            <div>
+              <div className='flex justify-center items-center mt-[156px] mb-6 '>
+                <img src='/images/illustration-empty.svg' className='h-full items-center'></img>
+              </div>
+              <div className='px-8 pb-52'>
+                <h2 className='text-white text-2xl text-center font-display'>Results shown here</h2>
+                <p className='text-Neutral-slate-500 text-center  pt-4 font-display'>Complete the form and click "calculate repayments" to see what your monthly repayments will be </p>
+              </div>
+            </div>
+
+
+          }
         </div>
       </div>
     </div>
-    // {/* </div> */}
   )
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,39 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../index.css'
 const Form = () => {
+    const [mortgageAmount, setMortgageAmount] = useState('');
+    const [interestRate, setInterestRate] = useState('');
+    const [mortgageTerm, setMortgageTerm] = useState('');
+    const [mortgageType, setMortgageType] = useState('');
+    const [interestOnly, setInterestOnly] = useState('');
+
+    const calculateRepayment = (e) => {
+        e.preventDefault();
+
+        console.log('hello')
+        if (mortgageAmount.length <= 0) {
+            alert('this filed is required');
+
+
+        }
+        else {
+            alert('not empty')
+        }
+    }
     return (
-        <div className='pt-12'>
-            <form>
-                <div className='relative'>
-                    <label for="mortgage amount" className='text-lg text-Neutral-slate-500 font-display font-medium'>Mortgage Amount</label>
-                    <input type='nunber' className='  border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-16 font-display font-semibold text-Neutral-slate-900 hover:border-Neutral-slate-900' />
-                    <span className='absolute left-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.6rem] font-display'><p className='px-4 py-3 font-bold text-Neutral-slate-500'>£</p></span>
-                </div>
-                <div className='py-6 flex justify-between space-x-6' >
+        <>
+            <div className='pt-12'>
+                <form onSubmit={calculateRepayment}>
                     <div className='relative'>
-                        <label for="mortgage Term" className='text-lg text-Neutral-slate-500 font-display font-medium'>Mortgage Term</label>
-                        <input type='nunber' className='  border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-4 font-display font-semibold text-Neutral-slate-900 ' />
-                        <span className='absolute right-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.6rem]'><p className='px-4 py-3 font-display font-bold text-Neutral-slate-500 '>years</p></span>
+                        <label htmlFor="mortgage amount" className='text-lg text-Neutral-slate-500 font-display font-medium'>Mortgage Amount</label>
+
+                        {/* { */}
+                        <>  < input type='text' value={mortgageAmount} onChange={(e) => setMortgageAmount(e.target.value)} className={`${mortgageAmount.length >= 0 ? 'border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-16 font-display font-semibold text-Neutral-slate-900 hover:border-Neutral-slate-900' : 'border border-primary-red rounded-md w-full h-12 my-2 pl-16 font-display font-semibold text-Neutral-slate-900 hover:border-Neutral-slate-900'}`} />
+                            <span className='absolute left-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.55rem] font-display'><p className='px-4 py-3 font-bold text-Neutral-slate-500'>£</p></span>
+                            <div className={`text-primary-red ${mortgageAmount.length >= 0 ? 'hidden' : 'block'}`}>This field is required</div>
+                        </>
+                        <></>
+                        {/* // <>
+                            //     <input type='text' value={mortgageAmount} onChange={(e) => setMortgageAmount(e.target.value)} className={`${mortgageAmount.length < 1 && 'border border-primary-red rounded-md w-full h-12 my-2 pl-16 font-display font-semibold text-Neutral-slate-900 hover:border-Neutral-slate-900'}`} />
+                            //     <span className='absolute left-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.6rem] font-display'><p className='px-4 py-3 font-bold text-Neutral-slate-500'>£</p></span>
+                            //     <div className={`text-primary-red ${mortgageAmount.length < 1 && 'block'}`}>This field is required</div>\
+                            // </> */}
+                        {/* } */}
                     </div>
-                    <div className='relative'>
-                        <label for="interest rate" className='text-lg text-Neutral-slate-500 font-display font-medium'>Interest Rate</label>
-                        <input type='nunber' className=' border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-4 font-display font-semibold text-Neutral-slate-900' />
-                        <span className='absolute right-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md top-[37px] '><p className='px-4 py-3 font-display font-bold text-Neutral-slate-500'>%</p></span>
+
+                    {/* <div className='py-6 flex justify-between space-x-6' >
+                        <div className='relative'>
+                            <label for="mortgage Term" className='text-lg text-Neutral-slate-500 font-display font-medium'>Mortgage Term</label>
+                            <input type='text' value={mortgageTerm} className='  border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-4 font-display font-semibold text-Neutral-slate-900 ' />
+                            <span className='absolute right-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md bottom-[0.6rem]'><p className='px-4 py-3 font-display font-bold text-Neutral-slate-500 '>years</p></span>
+
+                        </div>
+
+                        <div className='relative'>
+                            <label for="interest rate" className='text-lg text-Neutral-slate-500 font-display font-medium'>Interest Rate</label>
+                            <input type='text' value={interestRate} className=' border border-Neutral-slate-500 rounded-md w-full h-12 my-2 pl-4 font-display font-semibold text-Neutral-slate-900' />
+                            <span className='absolute right-0.5 bg-Neutral-slate-100 h-[46px] rounded-tl-md rounded-bl-md top-[37px] '><p className='px-4 py-3 font-display font-bold text-Neutral-slate-500'>%</p></span>
+                        </div>
+                    </div> */}
+                    <div>
+                        {/* <div className='h-12 w-full border border-Neutral-slate-500 rounded-md pt-2 pl-4 hover:border-primary-lime'>
+                            <input type='radio' value={mortgageType} className='h-4 w-4 ' />
+                            <label for="repayment" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Repayment</label>
+                        </div> */}
+                        {/* <div className='h-12 w-full border border-Neutral-slate-500 rounded-md my-2 pt-2 pl-4'>
+                            <input type='radio' value={interestOnly} className='h-4 w-4 text-Neutral-slate-500' />
+                            <label for="interest only" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Interset Only</label>
+                        </div> */}
                     </div>
-                </div>
-                <div>
-                    <div className='h-12 w-full border border-Neutral-slate-500 rounded-md pt-2 pl-4 hover:border-primary-lime'>
-                        <input type='radio' value='repayment' className='h-4 w-4 ' />
-                        <label for="repayment" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Repayment</label>
-                    </div>
-                    <div className='h-12 w-full border border-Neutral-slate-500 rounded-md my-2 pt-2 pl-4'>
-                        <input type='radio' value='interest' className='h-4 w-4 text-Neutral-slate-500' />
-                        <label for="interest only" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Interset Only</label>
-                    </div>
-                </div>
-                <button className='bg-primary-lime text-Neutral-slate-900 rounded-3xl w-3/4 h-14 font-semibold font-display my-10 hover:opacity-75'>Calculate Repayments</button>
-            </form>
-        </div>
+                    <button className='bg-primary-lime text-Neutral-slate-900 rounded-3xl w-3/4 h-14 font-semibold font-display my-10 hover:opacity-75' onClick={calculateRepayment}>Calculate Repayments</button>
+                </form>
+            </div>
+        </>
     )
 }
 
