aea50f480c9e9cdb3c7ae01e0458242c52981e61
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2024-11-25 23:31:20 +0100

    added style to radio button on selected

--- src/App.jsx
+++ src/App.jsx
@@ -46,21 +46,18 @@ function App() {
       const term = parseFloat(inputFields.mortgageTerm) * 12;
       let repayment;
       if (inputFields.mortgageType === 'repayment') {
-        repayment = (amount * rate) / (1 - Math.pow(1 + rate, -term))
-        console.log(repayment);
+        repayment = (amount * rate) / (1 - Math.pow(1 + rate, -term)).toLocaleString();
+        console.log(repayment.toLocaleString());
       } else if (inputFields.mortgageType === 'interestOnly') {
-        repayment = amount * rate;
+        repayment = (amount * rate)
         console.log(repayment);
       }
       setMonthlyRepayment(repayment ? repayment.toFixed(2) : null)
       console.log(repayment);
-
-
     })
-
-
     console.log(inputFields)
   }
+  const totalRepayment = monthlyRepayment * parseFloat(inputFields.mortgageTerm) * 12;
   // clear All function
   const clearAll = () => {
     setInputFields({
@@ -114,15 +111,17 @@ function App() {
                 </div>
               </div>
               <div>
-                <div className={`${inputFields.mortgageType === 'repayment' ? 'h-12 w-full border border-primary-lime rounded-md pt-2 pl-4' : 'h-12 w-full border border-Neutral-slate-500 rounded-md pt-2 pl-4 hover:border-primary-lime'}`} id='radio'>
-                  <input type='radio' value='repayment' name='mortgageType' onChange={handleChange} checked={inputFields.mortgageType === 'repayment'} className='w-4 h-4' />
-                  <label htmlFor="repayment" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Repayment</label>
-
+                <div className={`${inputFields.mortgageType === 'repayment' ? 'h-12 w-full border border-primary-lime bg-primary-lime bg-opacity-20 font-medium rounded-md pt-2 pl-4' : 'h-12 w-full border border-Neutral-slate-500 rounded-md pt-2 pl-4 hover:border-primary-lime '}`} >
+                  <div className={`${inputFields.mortgageType === 'repayment' ? 'border border-primary-lime h-4 w-4 rounded-full relative mt-2' : null}`}>
+                    <input type='radio' value='repayment' name='mortgageType' onChange={handleChange} checked={inputFields.mortgageType === 'repayment'} className={`${inputFields.mortgageType === 'repayment' ? 'h-2 w-2 bg-primary-lime appearance-none rounded-full absolute top-[3px] left-[3px]' : 'w-4 h-4'}`} />
+                    <label htmlFor="repayment" className={`${inputFields.mortgageType === 'repayment' ? 'text-lg text-Neutral-slate-900 font-display font-medium absolute left-7 -top-[6px]' : 'text-lg text-Neutral-slate-900 font-display font-medium pl-4'}`}>Repayment</label>
+                  </div>
                 </div>
-                <div className={`${inputFields.mortgageType === 'interestOnly' ? 'h-12 w-full border border-primary-lime rounded-md my-2 pt-2 pl-4' : 'h-12 w-full border border-Neutral-slate-500 rounded-md my-2 pt-2 pl-4'}`}>
-
-                  <input type="radio" value='interestOnly' name='mortgageType' onChange={handleChange} checked={inputFields.mortgageType === 'interestOnly'} className='h-4 w-4 text-Neutral-slate-500' id='radio' style={{ border: inputFields.mortgageType === "interestOnly" ? "1px solid red" : "white" }} />
-                  <label htmlFor="interest only" className='text-lg text-Neutral-slate-900 font-display font-medium pl-4'>Interest Only</label>
+                <div className={`${inputFields.mortgageType === 'interestOnly' ? 'h-12 w-full border border-primary-lime  bg-primary-lime bg-opacity-20 fount-medium rounded-md my-2 pt-2 pl-4' : 'h-12 w-full border border-Neutral-slate-500 rounded-md my-2 pt-2 pl-4'}`}>
+                  <div className={`${inputFields.mortgageType === 'interestOnly' ? 'border border-primary-lime h-4 w-4 rounded-full relative mt-2' : null}`}>
+                    <input type="radio" value='interestOnly' name='mortgageType' onChange={handleChange} checked={inputFields.mortgageType === 'interestOnly'} className={`${inputFields.mortgageType === 'interestOnly' ? 'h-2 w-2 bg-primary-lime appearance-none rounded-full absolute top-[3px] left-[3px]' : 'h-4 w-4 '}`} />
+                    <label htmlFor="interest only" className={`${inputFields.mortgageType === 'interestOnly' ? 'text-lg text-Neutral-slate-900 font-display font-medium absolute left-7 -top-[6px] ' : 'text-lg text-Neutral-slate-900 font-display font-medium pl-4'}`}>Interest Only</label>
+                  </div>
                 </div>
                 {errors.mortgageType ? (
                   <p className="error text-primary-red">This Field is required!</p>
@@ -141,10 +140,10 @@ function App() {
                 <div className='relative bg-primary-lime h-48 rounded-md w-full mt-14'>
                   <div className='absolute bg-Neutral-slate-900 shadow-lg rounded-md  top-[4px] w-full px-4'>
                     <p className='pt-10 pb-3 text-Neutral-slate-300 font-display'>Your monthly repayment</p>
-                    <h2 className='text-primary-lime text-4xl  font-display'>£ {monthlyRepayment}</h2>
+                    <h2 className='text-primary-lime text-4xl  font-display'>£{parseFloat(monthlyRepayment).toLocaleString()}</h2>
                     <hr className='my-6 text-Neutral-slate-300'></hr>
                     <p className='text-Neutral-slate-300 font-display pb-2 pt-[4px]'>Total you'll repay over the term</p>
-                    <p className='pb-8 text-Neutral-white text-lg font-display pt-[4px]'>£ </p>
+                    <p className='pb-8 text-Neutral-white text-lg font-display pt-[4px]'>£{totalRepayment.toLocaleString()} </p>
                   </div>
                 </div>
               </div>
